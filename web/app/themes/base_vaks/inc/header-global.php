9b4b67da560af1e7b0a19713a2fd2c3cb703c71f
Raivis Kalnins <raivis@63.lv>
2024-09-15 10:57:05 +0100

    Hero & patterns

---
+++ web/app/themes/base_vaks/inc/header-global.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Header Global / Meta - before </head>
+ */
+add_action('wp_head', 'header_global');
+
+function header_global() {
+?>
+	<!-- Header Caballero Global -->
+	
+	<!-- /Header Caballero Global -->
+<?php
+};
--- web/app/themes/base_vaks/patterns/_hero-home.php
+++ web/app/themes/base_vaks/patterns/_hero-home.php
@@ -11,7 +11,7 @@ $hero_h1 = $h['hero_title'] ?? '';
 if ( !empty($hero_h1) ) { 
 	$hero_t = '<h1 class="alignwide has-white-color wp-block-post-title">'.$hero_h1.'</h1>'; 
 } else { 
-	$hero_t = '<!-- wp:post-title {"level":1,"align":"wide","textColor":"white"} /-->';
+	$hero_t = '<!-- wp:post-title {"level":1,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} /-->';
 }
 $hero_btn = $h['hero_btn'] ?? '';
 if ( !empty($hero_btn) ) { 
@@ -21,16 +21,17 @@ if ( !empty($hero_btn) ) {
 }
 $hero_desc = $h['hero_desc'] ?? '';
 if ( !empty($hero_desc) ) {
-	$hero_d = '<!-- wp:caballero/post-acf-field {"fieldName":"hero_desc"} /-->'; 
+	$hero_d = '<!-- wp:paragraph --><p>'.$hero_desc.'</p><!-- /wp:paragraph -->';
 } else { 
 	$hero_d = '';
 }
 ?>
-<!-- wp:group {"tagName":"section","className":"wp-block-group hero hero-default} -->
-<section class="wp-block-group hero hero-default">
-	<!-- wp:group {"className":"container-boxed hero-wrap"} -->
+<!-- wp:group {"className":"hero hero-default"} -->
+<div class="wp-block-group hero hero-default">
+	<!-- wp:group {"className":"container-boxed hero-wrap","layout":{"type":"constrained"}} -->
 	<div class="wp-block-group container-boxed hero-wrap">
-		<!-- wp:caballero/post-acf-field {"fieldName":"hero_section"} /--><?php echo $hero_t.''.$hero_d.''.$hero_b ?></div>
+		<?=$hero_t?><?=$hero_d?><?=$hero_b?>
+	</div>
 	<!-- /wp:group -->
-</section>
+</div>
 <!-- /wp:group -->
