1c9451b1199706650d1275b34a6cab166d71b786
Raivis Kalnins <raivis@63.lv>
2024-09-17 00:28:19 +0100

    header & footer

--- web/app/themes/base_vaks/patterns/_footer-default.php
+++ web/app/themes/base_vaks/patterns/_footer-default.php
@@ -6,8 +6,8 @@
  * Keywords: Default Footer
  */
 ?>
-<!-- wp:group {"tagName":"div","className":"container-boxed","layout":{"inherit":true}} -->
-<div class="wp-block-group container">
+<!-- wp:group {"tagName":"div","className":"container foo","layout":{"inherit":true}} -->
+<div class="wp-block-group container foo">
 	<!-- wp:separator {"className":"is-style-wide"} -->
 	<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
 	<!-- /wp:separator -->
@@ -16,8 +16,8 @@
 		<!-- wp:spacer {"height":"20px"} -->
 		<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
 		<!-- /wp:spacer -->
-		<!-- wp:group {"tagName":"div","className":"foo-logo col col-25 col-l-100","layout":{"inherit":true}} -->
-		<div class="wp-block-group foo-logo col col-25 col-l-100">
+		<!-- wp:group {"tagName":"div","className":"foo-logo col col-40 col-l-100","layout":{"inherit":true}} -->
+		<div class="wp-block-group foo-logo col col-40 col-l-100">
 			<!-- wp:site-logo {"width":116,"shouldSyncIcon":false,"className":"is-style-default"} /-->
 			<!-- wp:html -->
 				<?php
@@ -31,11 +31,11 @@
 				$content = '
 					<!-- wp:social-links {"iconColor":"foreground","iconColorValue":"#000000","openInNewTab":true,"size":"has-small-icon-size","align":"center","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"left","flexWrap":"wrap"}} -->
 					<ul class="wp-block-social-links aligncenter has-small-icon-size has-icon-color is-style-logos-only">
-						<!-- wp:social-link {"url":"'.$yt.'","service":"youtube"} /-->
-						<!-- wp:social-link {"url":"'.$tw.'","service":"x"} /-->
+						<!-- wp:social-link {"url":"'.$ln.'","service":"linkedin"} /-->
 						<!-- wp:social-link {"url":"'.$fb.'","service":"facebook"} /-->
 						<!-- wp:social-link {"url":"'.$in.'","service":"instagram"} /-->
-						<!-- wp:social-link {"url":"'.$ln.'","service":"linkedin"} /-->
+						<!-- wp:social-link {"url":"'.$yt.'","service":"youtube"} /-->
+						<!-- wp:social-link {"url":"'.$tw.'","service":"x"} /-->
 					</ul>
 					<!-- /wp:social-links -->';
 				echo do_blocks($content);
@@ -43,8 +43,8 @@
 			<!-- /wp:html -->
 		</div>
 		<!-- /wp:group -->
-		<!-- wp:group {"tagName":"div","className":"foo-menu col col-75 col-l-100","layout":{"inherit":true}} -->
-		<div class="wp-block-group foo-menu col col-75 col-l-100">
+		<!-- wp:group {"tagName":"div","className":"foo-menu col col-60 col-l-100","layout":{"inherit":true}} -->
+		<div class="wp-block-group foo-menu col col-60 col-l-100">
 			<!-- wp:html -->
 				<?php
 				wp_nav_menu(array(
@@ -60,16 +60,16 @@
 		<!-- wp:spacer {"height":"20px"} -->
 		<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
 		<!-- /wp:spacer -->
-		<!-- wp:separator {"className":"is-style-default"} -->
-		<hr class="wp-block-separator has-alpha-channel-opacity is-style-default"/>
+		<!-- wp:separator {"className":"is-style-default col col-100"} -->
+		<hr class="wp-block-separator has-alpha-channel-opacity is-style-default col col-100"/>
 		<!-- /wp:separator -->
-		<!-- wp:spacer {"height":"20px"} -->
-		<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+		<!-- wp:spacer {"height":"5px"} -->
+		<div style="height:5px" aria-hidden="true" class="wp-block-spacer"></div>
 		<!-- /wp:spacer -->
-		<!-- wp:group {"tagName":"div","className":"foo-menu col col-100","layout":{"inherit":true}} -->
-		<div class="wp-block-group foo-menu col col-100">
+		<!-- wp:group {"tagName":"div","className":"foo-copy col col-100","layout":{"inherit":true}} -->
+		<div class="wp-block-group foo-copy col col-100">
 			<!-- wp:paragraph {"align":"left"} -->
-			<p class="has-text-align-left foo__copy">© <em>2023</em> VAKS. Visas tiesības rezervētas. <a rel="noreferrer noopener" href="https://caballero.lv" target="_blank" style="float:right"><img src="<?=get_template_directory_uri()?>/assets/img/svg/caballero-logo.svg" alt="Caballero" /></a><a  href="#cookies" target="_blank" style="float:right">Sīkdatnes</a><a  href="#terms" target="_blank" style="float:right">Privātuma politika</a></p>
+			<p class="has-text-align-left">© <em>2023</em> VAKS. Visas tiesības rezervētas. <a rel="noreferrer noopener" href="https://caballero.lv" target="_blank" style="float:right"><img src="<?=get_template_directory_uri()?>/assets/img/svg/caballero-logo.svg" alt="Caballero" /></a><a  href="#cookies" target="_blank" style="float:right">Sīkdatnes</a><a  href="#terms" target="_blank" style="float:right">Privātuma politika</a></p>
 			<!-- /wp:paragraph -->
 		</div>
 		<!-- /wp:group -->
